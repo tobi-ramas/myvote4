942b363d486c1f112e1af6c7f237340f7f981812
tobi-ramas <tobiramas12345@gmail.com>
2025-03-03 19:38:53 +0530

    Update click_button.py

--- click_button.py
+++ click_button.py
@@ -54,7 +54,7 @@ driver.execute_cdp_cmd('Page.addScriptToEvaluateOnNewDocument', {
 try:
     # Open the website
     driver.get('https://mycutebaby.in/contest/participant/679e77f65b140')
-    driver.add_cookie({'name': 'PHPSESSID', 'value': '9a3c0e24b65e58d0899603938cc86c07'})
+    driver.add_cookie({'name': 'PHPSESSID', 'value': 'c021455d044ba02cf461ed32a549412f'})
     time.sleep(2)
     # to refresh the page
     driver.refresh()
